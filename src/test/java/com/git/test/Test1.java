bee9f52de858a324295c5c5b3a0702db5beb20a7
Naveen <naveen.meduri5@gmail.com>
2020-10-25 19:23:41 +0530

    first commit

---
+++ src/test/java/com/git/test/Test1.java
@@ -0,0 +1,19 @@
+package com.git.test;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+  @Test
+  public void f1() {
+	  System.out.println("Test1");
+  }
+  
+  @Test
+  public void f2() {
+	  System.out.println("Test2");
+  }
+  @Test
+  public void f3() {
+	  System.out.println("Test3");
+  }
+}
